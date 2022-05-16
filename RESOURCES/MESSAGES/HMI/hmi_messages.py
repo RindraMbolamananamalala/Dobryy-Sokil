c5a2412a55d814b40f04de7b29301533ea5fcc08
RindraIBI <rindraibi@gmail.com>
2022-05-16 10:36:53 +0300

    Code Maintenance

--- RESOURCES/MESSAGES/HMI/hmi_messages.py
+++ RESOURCES/MESSAGES/HMI/hmi_messages.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""hmi_messages.py: The dedicated class file for all the String constants to be exposed to
+the User on the HMI
+"""
+
+__author__ = "Rindra Mbolamananamalala"
+__version__ = "1.0.1"
+__maintainer__ = "Rindra Mbolamananamalala"
+__email__ = "rindraibi@gmail.com"
+__status__ = "Prototype"
+
 class Messages:
     HMI_MAIN_WINDOW_TITLE = "Добрий Сокіл"
     HMI_BUTTON_LAUNCH_RESEARCH_TOOLTIP = "Go!"
