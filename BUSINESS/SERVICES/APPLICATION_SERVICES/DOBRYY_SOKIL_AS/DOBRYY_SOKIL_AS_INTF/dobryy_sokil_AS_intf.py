2f522252b307d7f8b93ce5d1e81d1f8d643edeb1
RindraIBI <rindraibi@gmail.com>
2022-07-12 11:57:10 +0300

    Dobryy Sokil Application Service (DEV Part)

--- BUSINESS/SERVICES/APPLICATION_SERVICES/DOBRYY_SOKIL_AS/DOBRYY_SOKIL_AS_INTF/dobryy_sokil_AS_intf.py
+++ BUSINESS/SERVICES/APPLICATION_SERVICES/DOBRYY_SOKIL_AS/DOBRYY_SOKIL_AS_INTF/dobryy_sokil_AS_intf.py
@@ -18,9 +18,16 @@ from abc import ABC, abstractmethod
 class DobryySokilASIntf(ABC):
 
     @abstractmethod
-    def hunt(self):
+    def hunt(self, object_to_search: str, root_folder_path: str):
         """
+        The Dobryy Sokil's M1 Level model representation of an actual hunt of a target prey in a given perimeter
+        in the Real World (M0) Level.
 
-        :return:
+        :param object_to_search: the "label" of the object to find, the current Model (M1) Level equivalent of the Real
+        World Level (M0) Sokil's prey
+        :param root_folder_path: root_folder_path: the "Root" folder's path for the search, the current Model (M1) Level
+        equivalent of the Real World Lever (M0) perimeter of search
+        :return: The list of Image Objects within the Root Folder that are related to the Object to Search, the current
+        Model (M1) Level equivalent of the Real World Level (M0) potential grabbed prey of the Sokil.
         """
         return
