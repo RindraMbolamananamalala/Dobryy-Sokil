44f6f23375150bfae1bd177f6d03db10d6568bc9
RindraIBI <rindraibi@gmail.com>
2022-07-25 11:41:48 +0300

    Software Engineering : CI/CD : Configuration of a CircleCI Pipeline

--- UTILS/image_utils.py
+++ UTILS/image_utils.py
@@ -24,7 +24,7 @@ def get_all_images_within_a_folder(folder_path: str) -> [str]:
         # A valid Folder path was provided
         possible_image_extensions = ["apng", "avif", "gif", "jpg", "jpeg", "jfif", "pjpeg", "pjp", "png", "svg", "webp"]
         images_to_return = []
-        LOGGER.info(os.listdir("/home/circleci/Добрый_Сокол/TESTS/UNIT_TESTS/RESOURCES"))
+        LOGGER.info(os.listdir("/home/circleci/Добрый_Сокол/TESTS/UNIT_TESTS/RESOURCES/IMAGE"))
         for folder_element in os.walk(folder_path):
             location_path = folder_element[0].replace("\\", "/")
             for file_element in folder_element[2]:
